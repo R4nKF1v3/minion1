1991d157b1e6c354b05392e8bcae55c3ffded94a
r4nkf1v3 <nahumoscatelli@gmail.com>
2019-09-26 12:39:17 +0000

    Bugfixes

--- scripts/Icons.gd
+++ scripts/Icons.gd
@@ -3,6 +3,7 @@ extends Node2D
 func _ready():
 	utils.connect("power_grabbed", self, "on_power_grabbed")
 	utils.connect("power_expired", self, "on_power_expired")
+	utils.connect("game_reset", self, "on_game_reset")
 
 func on_power_grabbed(pwr, time):
 	match pwr:
@@ -17,3 +18,7 @@ func on_power_expired(pwr):
 			$PWSJIcon.visible = false
 		utils.powers.extralife:
 			$PWELIcon.visible = false
+
+func on_game_reset():
+	for child in get_children():
+		child.visible = false
--- scripts/Score.gd
+++ scripts/Score.gd
@@ -4,6 +4,7 @@ var count = 0
 
 func _ready():
 	utils.connect("camera_elevated", self, "on_camera_elevation")
+	utils.connect("game_reset", self, "on_game_reset")
 	
 func on_camera_elevation(amount):
 	update_score(amount / 2)
@@ -11,3 +12,8 @@ func on_camera_elevation(amount):
 func update_score(amount):
 	count += amount
 	text = "Score: %d" % count
+
+func on_game_reset():
+	count = 0
+	text = "Score: %d" % count
+	
